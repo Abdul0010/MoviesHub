91a142151f52372b0f3512767948caba90eff204
abdul <abdulqaderutm@gmail.com>
2019-02-28 15:10:47 +0800

    fresh app

---
+++ app/src/main/java/uniapply/com/daggerretrofit/MainActivity.java
@@ -0,0 +1,13 @@
+package uniapply.com.daggerretrofit;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
